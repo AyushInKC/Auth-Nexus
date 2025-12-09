692ec4d5fdab4f4bb4f00af751d1e25cd7f324d3
AyushInKC <sirfayush1008@gmail.com>
2025-12-09 17:19:47 +0530

    Project Setup

---
+++ src/main/java/com/FourAM/Auth_Nexus/AuthNexusApplication.java
@@ -0,0 +1,13 @@
+package com.FourAM.Auth_Nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthNexusApplication.class, args);
+	}
+
+}
